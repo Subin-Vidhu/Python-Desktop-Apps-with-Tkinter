fb8f30314c129ed8c5d1562a50f918114a1ce93d
Subin-Vidhu <85066028+Subin-Vidhu@users.noreply.github.com>
2023-06-06 04:28:42 +0530

    message box

---
+++ tkinter/tkinter_messagebox/messagebox.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import messagebox
+
+#TKinter is intialized and we hide the window with
+root = Tk()
+root.withdraw()
+
+#show a message box
+messagebox.showinfo("Hello", "Hello World")
