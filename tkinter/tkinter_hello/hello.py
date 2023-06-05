a98ba9325690aa9c825f4e92c6e262700a44d73e
Subin-Vidhu <85066028+Subin-Vidhu@users.noreply.github.com>
2023-06-06 05:00:49 +0530

    label hello

---
+++ tkinter/tkinter_hello/hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import tkinter
+
+if __name__ == "__main__":
+    root = tkinter.Tk()
+    root.title("Hello World")
+    root.geometry("500x400")
+
+    # add label
+    label = tkinter.Label(root, text="hello")
+    label.pack(expand=tkinter.TRUE)
+    
+    root.mainloop()
