94e3a2c914a9a2a0e768c4e3ea5f6d5ab0b37fc9
Subin-Vidhu <85066028+Subin-Vidhu@users.noreply.github.com>
2023-06-06 04:50:18 +0530

    window

--- tkinter/tkinter_window/window.py
+++ tkinter/tkinter_window/window.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
+# To use tkinter, load it with import tkinter.  This works for Python 3 only.
 import tkinter
 
+# Then initialize the window with  root = tkinter.Tk().  You can set the title with  the function .title(name) and the size with .geometry().
+# Make Tkinter run with .mainloop().
 if __name__ == "__main__":
     root = tkinter.Tk()
     root.title("Title World")
