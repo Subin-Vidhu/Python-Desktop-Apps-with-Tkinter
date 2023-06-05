d508fe608016923008bd6594445057770a629e79
Subin-Vidhu <85066028+Subin-Vidhu@users.noreply.github.com>
2023-06-06 05:12:52 +0530

    button-oop

--- tkinter/tkinter_button/button-oop.py
+++ tkinter/tkinter_button/button-oop.py
@@ -7,13 +7,21 @@ class Demo1:
         self.master.geometry("200x200")
         self.master.title("Title")
 
-        self.btn = Button(self.master, text="Hello", command=self.func, width=20, height=10)
+        self.btn = Button(self.master, text="Hello", command=self.func, width=10, height=5)
         self.btn.place(x=0,y=0)
+
+        self.btn = Button(self.master, text="Quit", command=self.quit, width=10, height=5)
+        self.btn.place(x=0,y=100)
 
         self.frame.pack()
 
     def func(self):
         print('button click')
+    
+    def quit(self):
+        print('quit')
+        self.master.destroy()
+
         
 def main(): 
     root = Tk()
