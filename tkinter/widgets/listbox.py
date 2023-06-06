e2ae1b4e6634fed462ee44aa9e39f853010cb525
Subin-Vidhu <85066028+Subin-Vidhu@users.noreply.github.com>
2023-06-07 05:16:06 +0530

    listbox

--- tkinter/widgets/listbox.py
+++ tkinter/widgets/listbox.py
@@ -3,25 +3,25 @@
 import tkinter
 from tkinter import *
 
-def show():
-    print(Input1.get())
-    print(Input2.get())
-
 if __name__ == "__main__":
 
     root = Tk()
     root.geometry("200x200")
 
-    Label1 = Label(root, text="Name")
-    Label1.grid(row=0, column=0, pady = 2)
-    Input1 = Entry(root)
-    Input1.grid(row=0, column=1, pady = 2)
 
-    Label2 = Label(root, text="City")
-    Label2.grid(row=1, column=0, pady = 2)
-    Input2 = Entry(root)
-    Input2.grid(row=1, column=1, pady = 2)
+    Lb1 = Listbox(root)
 
-    Button(root, text='Show', command=show).grid(row=2, column=1, pady = 4)
+    Lb1.insert(1, "Python")
+    Lb1.insert(2, "Perl")
+    Lb1.insert(3, "C")
+    Lb1.insert(4, "PHP")
+    Lb1.insert(5, "JSP")
+
+    Lb1.grid(row=0, column=2, pady = 2)
+    Lb1.grid(row=1, column=2, pady = 2)
+    Lb1.grid(row=2, column=2, pady = 2)
+    Lb1.grid(row=3, column=2, pady = 2)
+    Lb1.grid(row=4, column=2, pady = 2)
+
 
     root.mainloop()
