f21975286b87cbfe365ad5e4b3d38103ca14040d
Subin-Vidhu <85066028+Subin-Vidhu@users.noreply.github.com>
2023-06-07 05:08:03 +0530

    input

--- tkinter/widgets/input.py
+++ tkinter/widgets/input.py
@@ -4,7 +4,8 @@ import tkinter
 from tkinter import *
 
 def click():
-    print(tinput.get())
+    print(tinput1.get())
+    print(tinput2.get())
     
 if __name__ == "__main__":
     root = tkinter.Tk()
@@ -15,12 +16,17 @@ if __name__ == "__main__":
     label = tkinter.Label(root, text="Enter text: ")
     label.grid(row=0,column=0)
 
+    label = tkinter.Label(root, text="Enter City: ")
+    label.grid(row=1,column=0)
+
     # add text input
-    tinput = Entry(root)
-    tinput.grid(row=0,column=1)
+    tinput1 = Entry(root)
+    tinput1.grid(row=0,column=1)
+    tinput2 = Entry(root)
+    tinput2.grid(row=1,column=1)
 
     # add button
     b = Button(root, text="Click", command=click)
-    b.grid(row=1,column=1)
+    b.grid(row=2,column=1)
     
     root.mainloop()
