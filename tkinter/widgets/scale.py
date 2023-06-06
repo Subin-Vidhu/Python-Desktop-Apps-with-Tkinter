14a9813331ea2b14f525ea23b15dbfe06ff4f20b
Subin-Vidhu <85066028+Subin-Vidhu@users.noreply.github.com>
2023-06-07 05:25:03 +0530

    scale

---
+++ tkinter/widgets/scale.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import tkinter
+from tkinter import *
+
+def func():
+    print(var.get())
+
+if __name__ == "__main__":
+    root = tkinter.Tk()
+    root.title("Title World")
+    root.geometry("300x300")
+
+    var = DoubleVar()
+    scale = Scale( root, variable = var )
+    scale.pack(anchor=CENTER)
+
+    button = Button(root, text="How much", command=func)
+    button.pack(anchor=CENTER)
+
+    root.mainloop()
