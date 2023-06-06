9d4083571b5a9322f43a45e8eeb712fae97a6f29
Subin-Vidhu <85066028+Subin-Vidhu@users.noreply.github.com>
2023-06-06 05:30:40 +0530

    image-basic

--- tkinter/tkinter_image/image-basic.py
+++ tkinter/tkinter_image/image-basic.py
@@ -4,8 +4,11 @@ root = Tk()
 root.geometry("640x480")
 root.title("Tkinter Window Title")
 
+# You can load images with PhotoImage() method
+# This can only load .gif, .pgm and .ppm
+
 #  GIF, PGM, and PPM
-imgicon = PhotoImage(file = r'image.gif')
+imgicon = PhotoImage(file = r'images.gif')
 img = Label(root, image=imgicon)
 img.place(x=0,y=0) 
 
