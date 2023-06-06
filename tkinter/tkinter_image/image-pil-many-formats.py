de3b7ee986e93795992b6d9d1038a553c6030ebe
Subin-Vidhu <85066028+Subin-Vidhu@users.noreply.github.com>
2023-06-06 05:35:37 +0530

    image-pil-many-formats

--- tkinter/tkinter_image/image-pil-many-formats.py
+++ tkinter/tkinter_image/image-pil-many-formats.py
@@ -1,11 +1,11 @@
 from tkinter import *
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk #Use PIL (Python Image Library) to load more image formats
 
 root = Tk()
 root.geometry("640x480")
 root.title("Tkinter Window Title")
 
-PIL_image = Image.open("image.gif")
+PIL_image = Image.open("1234.jpg")
 render = ImageTk.PhotoImage(PIL_image)
 img = Label(root, image=render)
 img.place(x=0,y=0) 
