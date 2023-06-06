1fdcc6262f8bca6e6d283f7188ce2a3984b32558
Subin-Vidhu <85066028+Subin-Vidhu@users.noreply.github.com>
2023-06-06 05:49:17 +0530

    pack-pad

--- tkinter/geometry_ pack/pack-pad.py
+++ tkinter/geometry_ pack/pack-pad.py
@@ -4,13 +4,13 @@ root = Tk()
 frame = Frame(root)
 frame.pack(fill=BOTH, expand=TRUE)
 
-l1 = Label(frame, text="Tkinter Label", bg="red", fg="white")
+l1 = Label(frame, text="Tkinter Label1", bg="red", fg="white")
 l1.pack(side=TOP, padx=10, pady=10)
 
-l2 = Label(frame, text="Tkinter Label", bg="yellow", fg="black")
+l2 = Label(frame, text="Tkinter Label2", bg="yellow", fg="black")
 l2.pack(side=LEFT, padx=10, pady=10)
 
-l3 = Label(frame, text="Tkinter Label", bg="blue", fg="white")
+l3 = Label(frame, text="Tkinter Label3", bg="blue", fg="white")
 l3.pack(side=LEFT, padx=10, pady=10)
 
 mainloop()
