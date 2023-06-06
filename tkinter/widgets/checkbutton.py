a0458c77e7d0775c32c55044315b7b4e2e87ea93
Subin-Vidhu <85066028+Subin-Vidhu@users.noreply.github.com>
2023-06-07 04:43:44 +0530

    checkbutton

--- tkinter/widgets/checkbutton.py
+++ tkinter/widgets/checkbutton.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     root.title("Tkinter")
     root.geometry("500x400")
 
-    var1 = tkinter.IntVar()
+    var1 = tkinter.IntVar() #Check boxes can be added to a Tkinter window. They are linked to a integer value, tkinter.IntVal().
     var2 = tkinter.IntVar()
     Checkbutton(root, text="Male", variable=var1).grid(row=0, sticky=W)
     Checkbutton(root, text="Female", variable=var2).grid(row=1, sticky=W)
