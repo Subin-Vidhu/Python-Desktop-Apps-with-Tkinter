f38af8f179d7863a30eaa27d0c3088005dcd1e45
Subin-Vidhu <85066028+Subin-Vidhu@users.noreply.github.com>
2023-06-07 04:45:55 +0530

    checkbutton2

---
+++ tkinter/widgets/checkbutton2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import tkinter
+from tkinter import *
+
+if __name__ == "__main__":
+    root = tkinter.Tk()
+    root.title("Tkinter")
+    root.geometry("500x400")
+
+    var1 = tkinter.IntVar(value=1)
+    var2 = tkinter.IntVar()
+    var3 = tkinter.IntVar()
+    var4 = tkinter.IntVar(value=1)
+
+    Checkbutton(root, text="Python", variable=var1, width=50, height=3).grid(row=0, sticky=W)
+    Checkbutton(root, text="Ruby", variable=var2, width=50, height=3).grid(row=1, sticky=W)
+    Checkbutton(root, text="C++", variable=var3, width=50, height=3).grid(row=2, sticky=W)
+    Checkbutton(root, text="C#", variable=var4, width=50, height=3).grid(row=3, sticky=W)
+    
+    root.mainloop()
