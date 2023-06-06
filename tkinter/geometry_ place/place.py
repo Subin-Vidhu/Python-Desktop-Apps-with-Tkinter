912778eb47d14a142460f55dddd293cd0ce3bcd8
Subin-Vidhu <85066028+Subin-Vidhu@users.noreply.github.com>
2023-06-06 06:02:23 +0530

    geometry-place

--- tkinter/geometry_ place/place.py
+++ tkinter/geometry_ place/place.py
@@ -3,11 +3,11 @@ from tkinter import *
 root = Tk()
 root.geometry("320x200")
 
-l1 = Label(root, text="Tkinter Label", bg="red", fg="white")
+l1 = Label(root, text="Tkinter Label1", bg="red", fg="white")
 l1.place(x = 0, y = 0)
-l2 = Label(root, text="Tkinter Label", bg="yellow", fg="black")
+l2 = Label(root, text="Tkinter Label2", bg="yellow", fg="black")
 l2.place(x = 0, y = 60)
-l3 = Label(root, text="Tkinter Label", bg="blue", fg="white")
+l3 = Label(root, text="Tkinter Label3", bg="blue", fg="white")
 l3.place(x = 200, y = 60)
 
 mainloop()
