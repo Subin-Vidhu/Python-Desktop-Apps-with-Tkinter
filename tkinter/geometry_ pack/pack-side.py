49c6a77730c3ae7f69f8974e22e7fbdd6af08c90
Subin-Vidhu <85066028+Subin-Vidhu@users.noreply.github.com>
2023-06-06 05:47:10 +0530

    pack-side

---
+++ tkinter/geometry_ pack/pack-side.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+root = Tk()
+frame = Frame(root)
+frame.pack()
+
+l1 = Label(frame, text="Tkinter Label1", bg="red", fg="white")
+l1.pack(side=TOP, fill=X)
+
+l2 = Label(frame, text="Tkinter Label2", bg="yellow", fg="black")
+l2.pack(side=LEFT)
+
+l3 = Label(frame, text="Tkinter Label3", bg="blue", fg="white")
+l3.pack(side=LEFT)
+
+mainloop()
