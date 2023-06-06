2295cec04ce59ac8431aeed0578ff8a250c69da3
Subin-Vidhu <85066028+Subin-Vidhu@users.noreply.github.com>
2023-06-07 04:59:30 +0530

    radio

--- tkinter/widgets/radio.py
+++ tkinter/widgets/radio.py
@@ -15,7 +15,8 @@ if __name__ == "__main__":
     
     var = IntVar()
     button1 = Radiobutton(root, text="Option 1", variable=var, value=1, command=callback)
-    button1.pack( anchor=W )
+    #You can set the position in the pack function, anchor = (W,E,S,N). If you leave it out it'll be in the middle.
+    button1.pack( anchor=W ) #pack the button to the left
     button2 = Radiobutton(root, text="Option 2", variable=var, value=2, command=callback)
     button2.pack( anchor=W )
 
