b519b2208bd9fb33cf83788e2262f04cf884347f
Subin-Vidhu <85066028+Subin-Vidhu@users.noreply.github.com>
2023-06-07 05:40:01 +0530

    spinbox range +ve working correctly

--- tkinter/widgets/spinbox.py
+++ tkinter/widgets/spinbox.py
@@ -1,25 +1,36 @@
-#!/usr/bin/python3
-
 import tkinter
 from tkinter import *
 
 def func():
-    # print(wb.get())
     value = int(wb.get())
     if value < 0 or value > 100:
         print("Value not in range")
+        bt.config(state=DISABLED)  # Disable the button
     else:
         print(value)
+        bt.config(state=NORMAL)  # Enable the button
+
+def validate_spinbox(value):
+    if value.isdigit():
+        if int(value) < 0 or int(value) > 100:
+            bt.config(state=DISABLED)  # Disable the button
+        else:
+            bt.config(state=NORMAL)  # Enable the button
+    return True
 
 if __name__ == "__main__":
     root = tkinter.Tk()
     root.title("Title World")
     root.geometry("300x300")
 
-    wb = Spinbox(root, from_=0, to=100)
+    vcmd = root.register(validate_spinbox)  # Register the validation function
+
+    bt = Button(root, text="click", command=func, state=DISABLED)  # Start with button disabled
+    bt.place(x=30, y=120)
+    
+    wb = Spinbox(root, from_=0, to=100, validate="key", validatecommand=(vcmd, "%P"))
     wb.place(x=30, y=100)
 
-    bt = Button(root, text="click", command=func)
-    bt.place(x=30, y=120)
+
 
     root.mainloop()
